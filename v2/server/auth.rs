88b48c9f26a5f0c30eea132f5c6e0bb12a6db5ba
Oleg Pustovit <oleg@nexo.sh>
2025-09-22 23:00:06 +0200

    add plan

---
+++ v2/server/auth.rs
@@ -0,0 +1,7 @@
+// TODO: implement crud
+// use jwt
+// login -> create jwt,
+// regiter -> create user
+// delete/deactivate account
+// mount user routes to the rest api
+// use d1_rs to do crud to sqlite
--- v2/server/src/database.rs
+++ v2/server/src/database.rs
@@ -1,7 +1,10 @@
+use crate::models;
+
 pub async fn init_db() -> Result<d1_rs::D1Client, Box<dyn std::error::Error + Send + Sync>> {
     let conn = rusqlite::Connection::open("db.sqlite3").unwrap();
-
     let db = d1_rs::D1Client::new_sqlite(conn);
-
+    let ac = d1_rs::auto_migration::AutoSchemaClient::new(db.clone());
+    ac.register_entity::<models::User>()?;
+    ac.auto_migrate().await?;
     Ok(db)
 }
